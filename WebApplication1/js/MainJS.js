7f363e6f1997bf08c813d0db471fe1bfd3980c76
brahafa <brachafab@gmail.com>
2015-06-08 14:39:06 +0300

    MainContent_answer1

--- WebApplication1/js/MainJS.js
+++ WebApplication1/js/MainJS.js
@@ -601,7 +601,8 @@ function findeCorectAns() {
 function validAmericanQ() {
     var idCheckChecked=0;
     StrAns = '';
-    if (document.getElementById('answer1').value.toString().length==0) {
+  
+    if (document.getElementById('MainContent_answer1').value.toString().length == 0) {
         document.getElementById('err').style.display = 'inline';
         document.getElementById('err').value = "מלא לפחות תשובה אחת*";
         return -1;
